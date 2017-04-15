d7775e880148f7ba9897c6f1efaf21974d75df2f
Samuel Rodriguez <surfer_samu@hotmail.com>
2017-04-15 15:03:50 -0400

    Update Admin.java

--- carfinder/src/carfinder/Admin.java
+++ carfinder/src/carfinder/Admin.java
@@ -5,30 +5,12 @@ public class Admin extends User{
 	public Admin(String username, String password, String name, String phone, String email){
 		super(username, password, name, phone, email);
 	}
-	
-	public boolean addCar(){
-		return false;
-		
-	}
-	
-	public boolean editCar(){
-		return false;
-		
-	}
-	
-	public String viewInventory(){
-		String invetory="";
-		
-		return invetory;	
-	}
 
 	@Override
 	public String toString() {
-		return "Username: "+getUsername()+ 
-				"\npassword: "+getPassword()+ 
-				"\nname: "+getName()+
-				"\nphone: "+getPhone()+
-				"\nemail: "+getEmail();
+		return "Name: "+getName()+
+				"\nPhone: "+getPhone()+
+				"\nEmail: "+getEmail();
 	}
 
 }
